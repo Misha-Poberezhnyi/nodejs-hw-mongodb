4e0f622f3a253084e426bc5ee8fb56dea77b953f
Misha_Pobereznyi <Pober1997@gmail.com>
2025-08-16 15:06:13 +0300

    HW4: hw4-validation

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -4,14 +4,43 @@ import { createContactService, deleteContactService, updateContactService } from
 
 export const getContactsAll = async (req, res) => {
 
-      const contacts = await Contact.find();
+  const page = parseInt(req.query.page) || 1;
+  const perPage = parseInt(req.query.perPage) || 10;
+
+  const skip = (page - 1) * perPage;
+
+  const allowedSortFields = ['name'];
+  const sortBy = allowedSortFields.includes(req.query.sortBy) ? req.query.sortBy : 'name';
+  const sortOrder = req.query.sortOrder === 'desc' ? -1 : 1;
+  const sortOptions = { [sortBy]: sortOrder };
+
+  const filter = {};
+
+  if (req.query.type) {
+    filter.contactType = req.query.type;
+  }
+  if (req.query.isFavourite !== undefined) {
+    filter.isFavourite = req.query.isFavourite === 'true';
+  }
+
+  const [contacts, totalItems] = await Promise.all([Contact.find().sort(sortOptions).skip(skip).limit(perPage), Contact.countDocuments(filter),]);
+
+  const totalPages = Math.ceil(totalItems / perPage);
 
       res.status(200).json({
         status: 200,
         message: 'Successfully found contacts!',
-        data: contacts,
+        data: {
+          data: contacts,
+          page,
+          perPage,
+          totalItems,
+          totalPages,
+          hasPreviousPage: page > 1,
+          hasNextPage: page < totalPages,
+        },
       });
-  };
+};
 
 export const getContactById = async (req, res) => {
   const { contactId } = req.params;
@@ -76,4 +105,4 @@ export const deleteContact = async (req, res) => {
   }
 
   res.status(204).send();
-}
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { getContactById, getContactsAll, createContact, updateContact, deleteContact } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getContactsAll));
-router.get('/:contactId', ctrlWrapper(getContactById));
-router.post('/', ctrlWrapper(createContact));
-router.patch('/:contactId', ctrlWrapper(updateContact));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactById));
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContact));
+router.patch('/:contactId', validateBody(updateContactSchema), ctrlWrapper(updateContact));
 router.delete('/:contactId', ctrlWrapper(deleteContact));
 
 export default router;
